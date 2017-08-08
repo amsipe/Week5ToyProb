fd31d15bf398e025a8eddf47526b2b01e1c33fc8
Anderson <asipe@redventures.com>
2017-08-08 17:26:00 -0400

    making changes

--- HTML-Form/js/app.js
+++ HTML-Form/js/app.js
@@ -1,36 +1,31 @@
 
 var pineapple = document.getElementById('pineapple');
 
+//function to check if pineapple option is "checked"
 var badPineapple = function(e) {
 
   if(pineapple.checked || pineapple.getAttribute('checked')){
-
+    //if pineapple is checked - alert user and remove "check" from DOM
     alert("Pineapple is not an available topping in your country. Please select another.");
     pineapple.checked = false;
     pineapple.removeAttribute('checked');
   }
 }
 
-
-
-var toppings = document.querySelectorAll('.toppings>label');
-
-
-
+var toppings = document.querySelectorAll('.toppings label');
+console.log(toppings);
+//loop through all available topping labels
 toppings.forEach(function (c) {
  var forBox = c.getAttribute('for');
  c.addEventListener('click', function (e) {
    var checkBox = document.querySelector('input[name='+forBox+']');
-
-
+   console.log(checkBox);
    if(checkBox.getAttribute("checked")) {
      checkBox.removeAttribute("checked");
    } else {
      checkBox.setAttribute("checked",true);
    }
-
-
  });
 })
-
+//listen for clicks on pinneapple option
 pineapple.addEventListener('click',badPineapple);
